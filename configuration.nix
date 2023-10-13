fe11fe83a7ca7dde75fbba0ec1be5acd5c5679b0
pricedown <josephmisaacs@gmail.com>
2023-10-13 14:11:20 -0400

    order redesign

--- configuration.nix
+++ configuration.nix
@@ -1,254 +1,251 @@
-# configuration.nix
-
 { config, pkgs, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
       ./hardware-configuration.nix
     ];
 
-  hardware = {
-    opengl.enable = true;
-  };
+  # ====== Network & Time ======
 
-  boot = {
-    loader = {
-      systemd-boot.enable = true;
-    };
-    kernelParams = [ "mem_sleep_default=deep" ];
-  };
-
-  networking = {
-    hostName = "jmhi-nixos";
-    networkmanager.enable = true;
-
-    # firewall.enable = false;
-    # firewall.allowedTCPPorts = [ ... ];
-    # firewall.allowedUDPPorts = [ ... ];
-  };
-
-  # Locale
+  networking.hostName = "nixos";
   time.timeZone = "America/New_York";
   i18n.defaultLocale = "en_US.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
 
-  # NVIDIA drivers
-  programs.hyprland.nvidiaPatches = true;
-  services.xserver.videoDrivers = [ "nvidia" ];
+  # ====== Hardware ======
+
+  boot.loader.systemd-boot.enable = true;
+  #boot.loader.grub.devices = [ "/dev/sda" ]; use grub or systemd-boot
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+
+  # enable audio & display
+  services.xserver.enable = true;
+  sound.enable = true;
+
+  # keyboard settings for xserver
+  services.xserver.layout = "us";
+  services.xserver.xkbVariant = "";
+
+  # enable nvidia drivers
+  #services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia.modesetting.enable = true;
+  hardware.opengl.enable = true;
+  hardware.opengl.driSupport32Bit = true;
 
-  hardware.nvidia.prime = {
-    offload = {
-      enable = true;
-      enableOffloadCmd = true;
-    };
-    nvidiaBusId = "PCI:1:0:0";
-    intelBusId = "PCI:0:2:0";
+  # optimize for laptop
+  #services.tlp.enable = true;
+  #services.power-profiles-daemon.enable = true;
+  #boot.kernelParams = [ "mem_sleep_default=deep" ];
+
+  # ignore lid for laptop server
+  services.logind.lidSwitch = "ignore";
+  services.logind.lidSwitchExternalPower = "ignore";
+
+  # ====== Security ======
+
+  services.openssh.enable = true; # enable remote login
+  security.rtkit.enable = true;
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [
+      80 # http
+      443 # wget
+      # SSH
+      22 # local port
+      6923 # local port
+      6922 # local port
+      # Minecraft
+      25565
+      25566
+      8123 # dynmap
+      24454 # voicemod
+    ];
+    allowedUDPPorts = [
+      # Minecraft
+      24454
+      25565
+      25566
+      8123
+    ];
   };
 
-  # -------------------
-  # Users & Environment
-  # -------------------
+  # ====== Users ======
 
-  nixpkgs.config.allowUnfree = true;
+  services.getty.autologinUser = "jmhi"; # automatically login
 
   users.users.jmhi = {
     isNormalUser = true;
     description = "Joseph Isaacs";
     extraGroups = [ "networkmanager" "wheel" ];
-
     packages = with pkgs; [
 
-      # Tools
+      # IDE
+      alacritty
+      jetbrains.rider
+      neovim
+      tmux
+      unityhub
+
+      # Desktop
+      firefox
+      gnome.nautilus
+      nomachine-client
+      obs-studio
       protonvpn-gui
-
-      # Firmware
       wootility
-      wooting-udev-rules
-      
-      # Media
-      discord
-      spotify
 
-      # Gaming
+      # Social
+      discord
       lunar-client
       lutris
       prismlauncher
+      spotify
       steam
-    ];
 
+      # Dependencies
+      wooting-udev-rules
+
+    ];
   };
 
+  # ====== Root ======
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  nixpkgs.config.allowUnfree = true;
   environment.systemPackages = with pkgs; [
-     # Hyprland
-     dbus
-     dunst
-     gtk3
-     hyprland-protocols
-     hyprland-share-picker
-     hyprpaper
-     kitty
-     libnotify
-     rofi-wayland
-     swww
-     wofi
-     xdg-desktop-portal-hyprland
-     wl-clipboard
-     wl-clipboard-x11
-     xclip
 
-     # waybar
-     (waybar.overrideAttrs (oldAttrs: {
-         mesonFlags = oldAttrs.mesonFlags ++ [" -Dexperimental=true "];
-       })
-     )
+    # Desktop
+    alacritty
+    firefox
+    flameshot
+    lxappearance
+    monitor
+    pavucontrol
+    transmission-qt
+    xterm
 
-     # Desktop Apps
-     alacritty
-     firefox
-     flameshot
-     lxappearance
-     monitor
-     pavucontrol
-     transmission-qt
+    # Command line
+    btop
+    curl
+    fzf
+    git
+    killall
+    man
+    neofetch
+    neovim
+    poppler_utils
+    ripgrep
+    rmlint
+    tmux
+    tree
+    unzip
+    wget
+    xclip
+    zip
 
-     # Terminal Apps
-     btop
-     curl
-     git
-     killall
-     man
-     neofetch
-     neovim
-     ripgrep
-     rmlint
-     tmux
-     tree
-     unzip
-     wget
-     xterm
-     # browsh with vim
-    #(callPackage (fetchgit {
-    #  url = "https://www.github.com/browsh-org/browsh.git";
-    #  ref = "vim-mode-branch";
-    #}) {})
+    # Virtualisation
+    bridge-utils
+    libvirt
+    qemu
+    qemu_kvm
+    virt-manager
+    x11docker
 
-    # Virtualization
-     virtualbox
-     bridge-utils
-     libvirt
-     qemu
-     qemu_kvm
-     virt-manager
+    # Coding utilities
+    cargo rustc rust-analyzer rustfmt
+    csslint
+    gcc clang clang-tools cmake cmake-format cmake-language-server
+    jdk openjdk17-bootstrap jre8
+    nixfmt
+    nodejs_20
+    python3Full
 
-     # Code
-     nixfmt
-     gcc clang clang-tools cmake cmake-format cmake-language-server
-     cargo rustc rust-analyzer rustfmt
-     python3Full
-     jdk openjdk17-bootstrap jre8
-     csslint
-     nodejs_20
-
-    # Theming
-    jetbrains-mono
-    font-awesome
   ];
 
-  environment.sessionVariables = {
-    WLR_NO_HARDWARE_CURSORS = "1";
-    NIXOS_OZONE_WL = "1";
-    EDITOR = "nvim";
-    WLR_RENDERER_ALLOW_SOFTWARE = "1 Hyprland";
+  environment.shellAliases = {
+    nixos-edit="su root -p -c 'cd /etc/nixos/ && nvim ./ -c 'NvimTreeFocus' '";
+    nixos-clean="sudo nix-collect-garbage -d && sudo nix profile wipe-history --profile /nix/var/nix/profiles/system --older-than 14d --extra-experimental-features nix-command";
+    nixos-evaluate="sudo nix-instantiate '<nixpkgs/nixos>' -A system";
+    nixos-switch="sudo nixos-rebuild switch";
+    nixos-upgrade="sudo nixos-rebuild switch --upgrade";
+
+    lspacks="nix-store --query --requisites /run/current-system";
+    shutdown="echo 'Do not hard shutdown the server without permission.'";
   };
 
-  # --------
-  # Programs
-  # --------
 
-  programs.hyprland = {
-    enable = true;
-    xwayland.enable = true;
-  };
+  # ====== Programs ======
 
-  # Boilerplate they said I might need idk
-  programs.mtr.enable = true;
+  networking.networkmanager.enable = true;
+
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
   };
 
-  # --------
-  # Services
-  # --------
-
-	
-  # Powersaving
-  services.power-profiles-daemon.enable = false;
-  services.thermald.enable = true;
-  services.tlp = {
-    enable = true;
-      settings = {
-	CPU_BOOST_ON_AC = 1;
-	CPU_BOOST_ON_BAT = 0;
-	CPU_SCALING_GOVERNOR_ON_AC = "performance";
-	CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-	START_CHARGE_THRESH_BAT0 = 75;
-	STOP_CHARGE_THRESH_BAT0 = 80;
-	RESTORE_THRESHOLDS_ON_BAT = 1;
-      };
-  };
-	
-  xdg.portal = {
-    enable = true;
-    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  users.extraGroups.vboxusers.members = [ "jmhi" ];
+  virtualisation.virtualbox = {
+    host.enable = true;
+    host.enableExtensionPack = true;
+    guest.x11 = true;
   };
 
-  services.openssh.enable = true;
+  # ====== Services ======
 
-  # Enable sound with pipewire
-  sound.enable = true;
-  security.rtkit.enable = true;
+  systemd.services = {
+    "getty@tty1".enable = false;
+    "autovt@tty1".enable = false;
+  };
+
+  # SSH
+  services.openssh = {
+    ports = [ 22 6923 ];
+    settings = {
+      PermitRootLogin = "yes";
+    };
+  };
+
+  # Display server
+  services.xserver = {
+    displayManager.defaultSession = "gnome-xorg";
+
+    desktopManager = { gnome.enable = true; };
+
+    displayManager = {
+      gdm.enable = true;
+      startx.enable = true; # allow manually starting xorg
+    };
+
+    windowManager = { i3.enable = true; };
+
+    deviceSection = ''
+      Option "TearFree" "true"
+      '';
+  };
+
+  # Sound server
+  hardware.pulseaudio.enable = false;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
-    pulse.enable =  true;
+    pulse.enable = true;
     jack.enable = true;
   };
 
-  services.xserver = {
-    enable = true;
-    layout = "us";
-    xkbVariant = "";
-
-    deviceSection = ''
-      Option "TearFree" "true"
-    '';
-
-    displayManager = {
-      gdm.enable = true;
-      gdm.wayland = true;
-    };
+  # Powersave for laptops
+  services.tlp.settings = {
+    CPU_BOOST_ON_AC = 1;
+    CPU_BOOST_ON_BAT = 0;
+    CPU_SCALING_GOVERNOR_ON_AC = "performance";
+    CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+    START_CHARGE_THRESH_BAT0 = 75;
+    STOP_CHARGE_THRESH_BAT0 = 80;
+    RESTORE_THRESHOLDS_ON_BAT = 1;
   };
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
-
+  # ====== Configuration version // Don't change ======
+  system.stateVersion = "23.05";
 }
